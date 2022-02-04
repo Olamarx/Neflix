cf67de93fb3ca0ca137039c30c3acd68fcae6f64
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-02-04 16:47:55 +0100

    Javascript modification

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// $('.faq-head').each(function(){
-//     $(this).click(function(){
-//         $(this).next().toggleClass('show-faq-content');
-//         let icon = $(this).children('span').children("i").attr('class');
-
-
-//         if(icon == "fas fa-plus"){
-//             $(this).children('span').html('<i class = "fa fa-times"></i>');
-//         } else {
-//             $(this).children('span').html('<i class = "fa fa-plus"></i>');
-//         }
-//     })
-// })
-
-
-// alert("Hi there");
-// confirm("I am gonna do something, okay?");
-// prompt("What should I do?");
-
-
 const fonts = document.querySelectorAll('.fonts');
 
 fonts.forEach(function(font){
