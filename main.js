3f4365119f493bb5130387f20e0b3584cc4386d3
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-02-04 18:20:42 +0100

    Javascript and CSS modification

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
-const fonts = document.querySelectorAll('.fonts');
+// Using selector inside the element
 
-fonts.forEach(function(font){
-font.addEventListener('click', function(e) {
-    const faxItem = e.currentTarget.parentElement.parentElement;
-    faxItem.classList.toggle("show-text");
+const faqItems = document.querySelectorAll(".faq-item");
+// console.log(faqItems)
+faqItems.forEach(function(faq){
+// console.log(faq);
+const fonts = faq.querySelector(".fonts");
+// console.log(fonts);
+fonts.addEventListener('click', function(){
+
+    faqItems.forEach(function(closeOthers){
+        if(closeOthers !== faq){
+            closeOthers.classList.remove("show-text");
+        }
+    })
+
+
+
+
+faq.classList.toggle('show-text');
 })
-})
+});
+
+
+
+
+
+// Tranversing the dom way
+
+// const fonts = document.querySelectorAll('.fonts');
+
+// fonts.forEach(function(font){
+// font.addEventListener('click', function(e) {
+//     const faqItem = e.currentTarget.parentElement.parentElement;
+//     faqItem.classList.toggle("show-text");
+// })
+// })
