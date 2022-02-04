88ab57e0e55f5de16ff56c0ec1dca44cc6024c56
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-02-04 06:04:40 +0100

    I just added the FAQ with the Javascript response at the opening.

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-$('.faq-head').each(function(){
-    $(this).click(function(){
-        $(this).next().toggleClass('show-faq-content');
-        let icon = $(this).children('span').children("i").attr('class');
+// $('.faq-head').each(function(){
+//     $(this).click(function(){
+//         $(this).next().toggleClass('show-faq-content');
+//         let icon = $(this).children('span').children("i").attr('class');
 
 
-        if(icon == "fas fa-plus"){
-            $(this).children('span').html('<i class = "fa fa-times"></i>');
-        } else {
-            $(this).children('span').html('<i class = "fa fa-plus"></i>');
-        }
-    })
+//         if(icon == "fas fa-plus"){
+//             $(this).children('span').html('<i class = "fa fa-times"></i>');
+//         } else {
+//             $(this).children('span').html('<i class = "fa fa-plus"></i>');
+//         }
+//     })
+// })
+
+
+// alert("Hi there");
+// confirm("I am gonna do something, okay?");
+// prompt("What should I do?");
+
+
+const fonts = document.querySelectorAll('.fonts');
+
+fonts.forEach(function(font){
+font.addEventListener('click', function(e) {
+    const faxItem = e.currentTarget.parentElement.parentElement;
+    faxItem.classList.toggle("show-text");
 })
-
-
-alert("Hi there");
-confirm("I am gonna do something, okay?");
-prompt("What should I do?");
-
+})
